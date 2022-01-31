c402c5a9276332ec552f5ebdfeeab1c73e6adecd
hburdge1 <95104319+hburdge1@users.noreply.github.com>
2022-01-31 15:01:27 -0800

    commit tree fb4ab5be316346b81b8a59c065330a351b202648

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
 
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :genre 
+    attr_accessor :page_count
 
+    def initialize(title, author, genre, page_count)
+        @title = "#{title}"
+        @author = author
+        @page_count=page_count
+        @genre=genre
+    
+
+    def turn_page(book)
+       puts "Flipping the page...wow, you read fast!"
+    end
+end
+end
